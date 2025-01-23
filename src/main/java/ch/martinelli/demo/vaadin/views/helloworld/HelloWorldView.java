3296a7f32d66d915e34c86ab8f8a01b873acb769
Simon Martinelli <simon@martinelli.ch>
2025-01-23 15:23:38 +0000

    Formatting

--- src/main/java/ch/martinelli/demo/vaadin/views/helloworld/HelloWorldView.java
+++ src/main/java/ch/martinelli/demo/vaadin/views/helloworld/HelloWorldView.java
@@ -3,7 +3,6 @@ package ch.martinelli.demo.vaadin.views.helloworld;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Menu;
@@ -22,7 +21,9 @@ public class HelloWorldView extends VerticalLayout {
         TextField nameTextField = new TextField("Your name");
 
         Button greetButton = new Button("Say hello");
-        greetButton.addClickListener(e -> Notification.show("Hello %s".formatted(nameTextField.getValue()), 3000, Notification.Position.TOP_CENTER));
+        greetButton.addClickListener(e ->
+                Notification.show("Hello %s".formatted(nameTextField.getValue()),
+                        3000, Notification.Position.TOP_CENTER));
         greetButton.addClickShortcut(Key.ENTER);
 
         add(nameTextField, greetButton);
